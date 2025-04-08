72f9355fd66c200e0353cb7c5cde9b42c8216aa9
RazaMuhammad5758 <waistband161@gmail.com>
2025-04-08 23:22:08 +0500

    authentication

--- index.js
+++ index.js
@@ -4,10 +4,13 @@ const PORT = 7000
 const app = express()
 const userRoutes = require('./routes/user')
 const mongoose = require("mongoose")
+const cookieParser = require("cookie-parser")
+const { checkForAuthenticationToken } = require("./middlewares/authentication")
 
 app.set("view engine", "ejs")
 app.set("views", path.resolve("./views"))
-
+app.use(cookieParser())
+app.use(checkForAuthenticationToken('token'))
 app.use(express.urlencoded({extended: false}))
 
 mongoose.connect('mongodb://localhost:27017/blogify').then(e=>{console.log("Mongodb connected")})
---
+++ middlewares/authentication.js
@@ -0,0 +1,16 @@
+const { validateToken } = require("../services/authentication")
+
+function checkForAuthenticationToken(cookieName){
+    return (req, res, next)=>{
+        const tokenCookieValue = req.cookies[cookieName]
+        if(!tokenCookieValue){
+            next()
+        }
+        try{
+            const userPayload =  validateToken(tokenCookieValue)
+            req.user = userPayload
+        } catch(error){}
+        next()
+    }
+}
+module.exports = {checkForAuthenticationToken}
