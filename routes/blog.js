5d9df09414de7a8312a743676f453e7bdc181cc0
RazaMuhammad5758 <waistband161@gmail.com>
2025-04-13 22:24:00 +0500

    adding comment section

--- routes/blog.js
+++ routes/blog.js
@@ -3,6 +3,7 @@ const Blog = require("../models/blog")
 const multer = require("multer")
 const path = require("path")
 const { findById } = require("../models/user")
+const Comment = require("../models/comments")
 
 const router = Router()
 
@@ -27,12 +28,21 @@ router.get("/add-new", (req, res)=>{
 const upload = multer({ storage: storage })
 
 router.get("/:id", async (req, res)=>{
-    const blog = await  Blog.findById(req.params.id)
+    const blog = await  Blog.findById(req.params.id).populate("createdBy")
     return res.render('blog',{
         user: req.user,
         blog,
         
     })
+})
+
+router.post("/comment/:blogId", async (req, res)=>{
+     await Comment.create({
+        content: req.body.content,
+        blogId: req.params.blogId,
+        createdBy: req.user._id,
+    });
+    return res.redirect(`/blog/${req.params.blogId}`)
 })
 
 
